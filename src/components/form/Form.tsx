47bf709c03f22058527f4c883fbffe4fef6c0f54
Olapat <lapatbr19@gmail.com>
2022-05-04 01:08:06 +0700

    Update useForm type
    add type values

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -1,30 +1,30 @@
-import React, { memo, createContext, useCallback } from 'react'
+import React, { createContext, useCallback } from 'react'
 import type { UseFormType } from '../../useForm/useForm'
 import middleware from '../../utils/middleware'
 
 export const FormContext = createContext({})
 
-interface Props extends React.FormHTMLAttributes<HTMLFormElement> {
+interface Props<ValuesType> extends React.FormHTMLAttributes<HTMLFormElement> {
   children: React.ReactNode;
-  handlerSubmit: Function;
-  form: UseFormType;
+  handlerSubmit:| ((values: ValuesType, next: Function, end: Function) => any) | Function[];
+  form: UseFormType<ValuesType>;
   onSubmitError?: Function;
 }
 
-const Form: React.FC<Props> = (props) => {
+const Form = <ValuesType extends { [key: string]: any } = {}>(props: Props<ValuesType>) => {
   const { children, handlerSubmit, onSubmitError, form, ...formProps } = props
   const { setSubmitting, validate, values, blackList, whiteList, rules } = form
 
-  const onStartSubmit = (next: Function) => {
+  const onStartSubmit = React.useCallback((next: Function) => {
     setSubmitting(true)
     if (typeof next === 'function') {
       next()
     }
-  }
+  }, [setSubmitting])
 
-  const onEndSubmit = () => {
+  const onEndSubmit = React.useCallback(() => {
     setSubmitting(false)
-  }
+  }, [setSubmitting])
 
   const buildValidate = useCallback((next: Function, end: Function) => {
     const resultValid = validate(next, end, true)
@@ -45,7 +45,7 @@ const Form: React.FC<Props> = (props) => {
     } else if (typeof handlerSubmit === 'function') {
       middleware([onStartSubmit, buildValidate, handlerSubmit, onEndSubmit], { end: onEndSubmit })
     }
-  }, [handlerSubmit, buildValidate])
+  }, [handlerSubmit, buildValidate, onEndSubmit, onStartSubmit])
 
   const isFieldDisable = useCallback((name):boolean => {
     if (blackList === '*') {
@@ -70,4 +70,4 @@ const Form: React.FC<Props> = (props) => {
   )
 }
 
-export default memo(Form)
+export default Form
