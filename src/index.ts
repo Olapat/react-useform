207743ae75886934aca6b065fe1d7c027e10e8f2
Olapat <lapatbr19@gmail.com>
2022-08-09 00:57:53 +0700

    Change export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { useForm } from './useForm'
+export { useForm } from './useForm'
 export type { UseFormType, ErrorForm, Rules } from './useForm/useForm'
 export { useFormList } from './useFormList'
 export type { UseListType, ValuesList } from './useFormList/useList'
@@ -6,4 +6,3 @@ export { default as Form, FormContext } from './useForm/Form'
 export type { FormContextType } from './useForm/Form'
 export { getMessageReq } from './utils/field'
 
-export default useForm
