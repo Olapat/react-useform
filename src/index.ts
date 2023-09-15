719a5c7961cafc92b9c5cf09e496ee295586f4a6
Olapat <lapatbr19@gmail.com>
2023-09-15 08:29:15 +0700

    Update to export useList

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { useForm } from './useForm'
 export type { UseFormType, ErrorForm, Rules } from './useForm/useForm'
-export { useFormList } from './useFormList'
+export { useFormList, useList } from './useFormList'
 export type { UseListType, ValuesList } from './useFormList/useList'
 export { default as Form, FormContext } from './useForm/Form'
 export type { FormContextType } from './useForm/Form'
--- src/useFormList/index.ts
+++ src/useFormList/index.ts
@@ -1,1 +1,2 @@
 export { default as useFormList } from './useFormList'
+export { default as useList } from './useList'
